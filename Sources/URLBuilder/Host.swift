6feff0fceffa92d4f2b267bf455f0139b9deeffc
Tim Wolff <timtwotoes@me.com>
2022-10-26 20:44:01 +0200

    A little refactoring

--- Sources/URLBuilder/Host.swift
+++ Sources/URLBuilder/Host.swift
@@ -8,16 +8,24 @@
 import Foundation
 
 public struct Host: URLComponentsCompatible {
+    private static let hostRegExPattern = try! NSRegularExpression(pattern: #"^(?:https?://)?(?:[^@\/\n]+@)?(?:www\.)?([^:/?\n]+)"#)
     private let name: String
+
+    private static func extractHost(from string: String) -> String {
+        let range = NSRange(string.startIndex..<string.endIndex, in: string)
+        if let match = hostRegExPattern.firstMatch(in: string, range: range) {
+            if let hostRange = Range(match.range(at: 1), in: string) {
+                return String(string[hostRange])
+            } else {
+                return ""
+            }
+        } else {
+            return ""
+        }
+    }
     
     public init(_ name: String) {
-        let expression = try! NSRegularExpression(pattern: #"^(?:https?://)?(?:[^@\/\n]+@)?(?:www\.)?([^:/?\n]+)"#)
-        let range = NSRange(name.startIndex..<name.endIndex, in: name)
-        if let match = expression.firstMatch(in: name, range: range) {
-            self.name = String(name[Range(match.range(at: 1), in: name)!])
-        } else {
-            self.name = ""
-        }
+        self.name = Host.extractHost(from: name)
     }
     
     public func update(_ components: inout URLComponents) {
